e5760f9dcbe58c839d99f3339a0096f3a50122cc
nyambura00 <winnienyambura14@gmail.com>
2022-01-18 10:10:39 -0500

    implement search by filtering

--- src/Table.js
+++ src/Table.js
@@ -22,7 +22,7 @@ export default function Table({ columns, data }) {
 
   const handleFilterChange = e => {
     const value = e.target.value || undefined;
-    setFilter("show.name", value);
+    setFilter("show.name", value); //search functionality that filters show names
     setFilterInput(value);
   };
 
