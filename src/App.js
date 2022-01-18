05fb421e5f02e8fc7c8576e71f670ce7c970a038
nyambura00 <winnienyambura14@gmail.com>
2022-01-18 09:50:42 -0500

    add styled-components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import axios from 'axios';
 
 import Table from "./Table";
 
+import styled from 'styled-components'
+
 function App() {
 
+  //dealing with data
   const [data, setData] = useState([]); //initial data state for storing API data
 
   //Once mounted, useEffect initiates the API to set data
@@ -60,9 +63,37 @@ function App() {
     []
   );
 
+  //styling the table
+  const StyledTable = styled.div`
+    table {
+      width: 100%;
+      border-spacing: 0;
+      border: 1px solid black;
+      tr {
+        :last-child {
+          td {
+            border-bottom: 0;
+          }
+        }
+      }
+      th,
+      td {
+        margin: 0;
+        padding: 1rem;
+        border-bottom: 1px solid black;
+        border-right: 1px solid black;
+        :last-child {
+          border-right: 0;
+        }
+      }
+    }
+  `
+
   return (
     <div className="App">
-      <Table columns={columns} data={data} />
+      <StyledTable>
+        <Table columns={columns} data={data} />
+      </StyledTable>
     </div>
   );
 }
