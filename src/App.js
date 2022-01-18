8f6ee5c2a17f0bf26b03859d7cf5e459f13f15ae
nyambura00 <winnienyambura14@gmail.com>
2022-01-17 20:03:38 -0500

    initializing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Table from "./Table";
 
 function App() {
+
+  const [data, setData] = useState([]); //initial data state for storing API data
+
+  //Once mounted, useEffect initiates the API to set data
+  useEffect(() => {
+    (async () => {
+      const result = await axios("https://api.tvmaze.com/search/shows?q=snow");
+      setData(result.data);
+    })();
+  }, []);
+
+  //the column object
+  const columns = useMemo(
+    () => [
+      {
+        // first group - TV Show
+        Header: "TV Show",
+        // First group columns
+        columns: [
+          {
+            Header: "Name",
+            accessor: "show.name"
+          },
+          {
+            Header: "Type",
+            accessor: "show.type"
+          }
+        ]
+      },
+      {
+        // Second group - Details
+        Header: "Details",
+        // Second group columns
+        columns: [
+          {
+            Header: "Language",
+            accessor: "show.language"
+          },
+          {
+            Header: "Genre(s)",
+            accessor: "show.genres"
+          },
+          {
+            Header: "Runtime",
+            accessor: "show.runtime"
+          },
+          {
+            Header: "Status",
+            accessor: "show.status"
+          }
+        ]
+      }
+    ],
+    []
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table column={columns} data={data} />
     </div>
   );
 }
---
+++ src/Table.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { useTable, useFilters, useSortBy } from "react-table";
+
+export default function Table({ columns, data }) {
+  const [filterInput, setFilterInput] = useState("");
+  // Using the state and functions returned from useTable to build UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    setFilter
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useFilters,
+    useSortBy
+  );
+
+  const handleFilterChange = e => {
+    const value = e.target.value || undefined;
+    setFilter("show.name", value);
+    setFilterInput(value);
+  };
+
+  // Rendering the table UI
+  return (
+    <>
+      <input
+        value={filterInput}
+        onChange={handleFilterChange}
+        placeholder={"Search name"}
+      />
+      <table {...getTableProps()}>
+        <thead>
+        {headerGroups
+            ? headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                    <th
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    className={
+                        column.isSorted
+                        ? column.isSortedDesc
+                            ? "sort-desc"
+                            : "sort-asc"
+                        : ""
+                    }
+                    >
+                    {column.render("Header")}
+                    </th>
+                ))}
+                </tr>
+                ))
+            :"Loading..."}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows
+            ? rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+            })
+            :"Loading ..."}
+        </tbody>
+      </table>
+    </>
+  );
+}
